b56efcd5225403a1fc34e114fe8ef242e8bb9b8f
Flo <florent.vouin@gmail.com>
2023-08-25 11:59:08 +0200

    🐛 Simplify pubsub_topic_ids expression to allow Terraform to detect minimal dependencies

--- pubsub.tf
+++ pubsub.tf
@@ -6,13 +6,11 @@ locals {
     [for trigger in values(local.pubsub_triggers) : trigger.topic],
   )
 
-  # The map between event full names and Pub/Sub topic IDs, only for referenced topics.
-  # This defaults to topics in the set GCP project, but can be overridden by the user.
-  pubsub_topic_ids = {
-    for topic in local.referenced_topics :
-    topic => coalesce(
-      try(var.pubsub_topic_ids[topic], null),
-      "projects/${local.gcp_project_id}/topics/${topic}"
-    )
-  }
+  # The map between event full names and Pub/Sub topic IDs.
+  # This defaults to topics in the set GCP project, but can be overridden by the user (especially usefull to express the
+  # dependency on the Pub/Sub topic resources).
+  pubsub_topic_ids = merge(
+    { for topic in local.referenced_topics : topic => "projects/${local.gcp_project_id}/topics/${topic}" },
+    var.pubsub_topic_ids,
+  )
 }
